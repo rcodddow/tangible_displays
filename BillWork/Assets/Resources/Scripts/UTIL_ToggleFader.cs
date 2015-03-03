2205e19f1546a3343584bf5ffce0e78ce85fbca8
RobShewaga <robshewaga@gmail.com>
2015-03-03 18:17:17 +0900

    Updated tabletop
    -Added a toggle fading script so that toggled fade when on/off.
    -Changed some buttons to toggles for better feedback.
    -Grouped toggles together so that only one can be active as once.

---
+++ BillWork/Assets/Resources/Scripts/UTIL_ToggleFader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UTIL_ToggleFader:MonoBehaviour{
+	public Color colorOn;		//The normal color for when the toggle is on.
+	public Color colorOff;		//The normal color for when the toggle if off.
+	public bool state;			//The current state of the toggle.
+
+	private bool updated;		//Whether the color needs to be updated this frame or not.
+
+	void Start(){
+		updated=false;
+	}
+
+	void Update(){
+		UpdateFade();
+	}
+
+	public void UpdateFade(){
+		//Set the fade state of the toggle.
+		if(GetComponent<Toggle>()!=null){
+			if(state!=GetComponent<Toggle>().isOn) updated=true;
+			state=GetComponent<Toggle>().isOn;
+		}
+
+		if((GetComponent<Image>()!=null)&&(updated)){
+			if(state) GetComponent<Image>().color=colorOn;
+			else GetComponent<Image>().color=colorOff;
+			updated=false;
+		}
+
+	}
+}
