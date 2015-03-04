1a5b10ec304888d7fa6be67d02f3b5badbfe2d5a
RobShewaga <robshewaga@gmail.com>
2015-03-04 10:31:28 +0900

    Updated tabletop
    -Added the eye images to a scope canvas with a simple controlling
    script.
    -Created a net handler with a tabletop state container which can
    send JSON messages.

---
+++ BillWork/Assets/Resources/Scripts/NET_Handler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NET_Handler:MonoBehaviour{
+	public NET_TableState net_TableState;		//The current state of the table version of the program.
+	
+	public UTIL_Scripts scripts;
+
+	void Start(){
+		net_TableState=new NET_TableState(scripts.ui_LayersMenuHandler.root);	//Create the TableState.
+	}
+
+	void Update(){
+		//Update the different net handlers.
+		net_TableState.Update();
+	}
+	private void SendBroadcast(){
+		//Send out the broadcast messages.
+	}
+}
